e556cda445fae7f3f09fb8aa7cb87693f09a4daa
Priya <priyamaheshwari183@gmail.com>
2023-07-09 20:32:15 +0530

    Added Database

--- dashboard.php
+++ dashboard.php
@@ -3,44 +3,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Dashboard | PG Life</title>
 
-        <link href="css/bootstrap.min.css" rel="stylesheet" />
-        <link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet" />
-        <link href="css/common.css" rel="stylesheet" />
+        <?php
+        include "includes/head_links.php";
+        ?>
         <link href="css/dashboard.css" rel="stylesheet" />
     </head>
 
     <body>
-        <div class="header sticky-top">
-            <nav class="navbar navbar-expand-md navbar-light">
-                <a class="navbar-brand" href="index.html">
-                    <img src="img/logo.png" />
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-    
-                <div class="collapse navbar-collapse justify-content-end" id="my-navbar">
-                    <ul class="navbar-nav">
-                        <div class="nav-name"> Hi, Priya</div>
-                        <li class="nav-item">
-                            <a class="nav-link" href="dashboard.html">
-                                <i class="fas fa-user"></i> Dashboard
-                            </a>
-                        </li>
-                        <div class="nav-vl"></div>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" data-toggle="modal" data-target="#login-modal">
-                                <i class="fas fa-sign-in-alt"></i> Login
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-
-        <div id="loading">
-        </div>
+        <?php
+        include "includes/head_links.php";
+        ?>
 
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb py-2">
@@ -153,26 +125,9 @@
                 </div>
             </div>
         </div>
-    
-        <div class="footer">
-            <div class="page-container footer-container">
-                <div class="footer-cities">
-                    <div class="footer-city">
-                        <a href="property_list.html">PG in Delhi</a>
-                    </div>
-                    <div class="footer-city">
-                        <a href="property_list.html">PG in Mumbai</a>
-                    </div>
-                    <div class="footer-city">
-                        <a href="property_list.html">PG in Bangalore</a>
-                    </div>
-                    <div class="footer-city">
-                        <a href="property_list.html">PG in Hyderabad</a>
-                    </div>
-                </div>
-                <div class="footer-copyright">© 2020 Copyright PG Life </div>
-            </div>
-        </div>
 
+        <?php
+        include "includes/footer.php";
+        ?>
     </body>
 </html>
